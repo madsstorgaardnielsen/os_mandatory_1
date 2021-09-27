bb237e732569454520f787330eebc1a098d7904d
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2021-09-27 20:25:33 +0200

    -

--- myshell1.c
+++ myshell1.c
@@ -1,20 +1,40 @@
-#include <stdlib.h>
+
 #include <stdio.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-char input[1024];
-char* arg1[512];
-char* arg2[512];
+static char lineInput[1024];
 
-int main(int argc, char *argv[])
-{
-while (1)
-{
+static char *args1[512];
+static char *args2[512];
 
-}
+int main(int argc, char *argv[]) {
+  while (1) {
+    char dir[FILENAME_MAX];
+    getcwd(dir, sizeof(dir));
+    printf("%s: ", dir);
 
+    pid_t pid;
+    int pipefd[2];
 
+    pipe(pipefd);
+
+    if (fgets(lineInput, 1024, stdin) == NULL) {
+      return 0;
+    }
+
+    char delimiter[] = "|";
+    char *command = strtok(lineInput, delimiter);
+    int index1 = 0;
+    args1[index1] = command;
+
+    if (args1[0] != NULL && strcmp(args1[0], "exit") == 0) {
+      exit(0);
+    }
+
+    printf("%s\n",  args1[index1]);
+  }
 }
