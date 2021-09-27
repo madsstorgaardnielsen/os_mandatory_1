e7c25372d3036e4ed62093463610f6cfcad7cf91
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2021-09-27 20:38:08 +0200

    -

--- myshell1.c
+++ myshell1.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,68 +5,98 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-static char lineInput[1024];
+static char userInput[1024];
 
-static char *args1[512];
-static char *args2[512];
-int getInputCommands(char *args[]){
-    //Get the command with getline()
-    char *line = NULL;//The chars of the line
-    size_t len =0;//buffer length
-    getline(&line,&len,stdin);//Reads from stdin
-
-        //Get the rest of the tokens into array args  
-    char delimit[] = {'|'};
-    args[0] = strtok(line,delimit);
-    char *p =args[0];
-    int i = 1;
-
-    //Get the rest of the args - last element will be NULL
-    while (p != NULL){
-    	p = strtok(NULL,delimit);
-	    args[i++] = p;
-    }
-    return i-1;
-}
-
-
-
-int getArgs(char *input, char *args[]){
-    //Get the rest of the tokens into array args  
-    char delimit[] = {' ','\n'};
-    args[0] = strtok(input,delimit);
-    char *p =args[0];
-    int i = 1;
-
-    //Get the rest of the args - last element will be NULL
-    while (p != NULL){
-    	p = strtok(NULL,delimit);
-	    args[i++] = p;
-    }
-    return 1;
-}
-
+static char *arg1[512];
+static char *arg2[512];
+void executeCommand(pid_t pid, int pipefileDesc[2], int pipeExist);
 
 int main(int argc, char *argv[]) {
   while (1) {
-    char dir[FILENAME_MAX];
-    getcwd(dir, sizeof(dir));
-    printf("%s: ", dir);
-
+    printf("$ ");
     pid_t pid;
-    int pipefd[2];
+    int pipefileDesc[2];
 
-    pipe(pipefd);
+    pipe(pipefileDesc);
 
-    if (fgets(lineInput, 1024, stdin) == NULL) {
+    if (fgets(userInput, 1024, stdin) == NULL) {
       return 0;
     }
 
-    if (args1[0] != NULL && strcmp(args1[0], "exit") == 0) {
+    char delimit[] = "\n ";
+    char *command = strtok(userInput, delimit);
+    int argIndex1 = 0;
+    arg1[argIndex1] = command;
+
+    while (command != NULL) {
+      command = strtok(NULL, delimit);
+
+      argIndex1++;
+      arg1[argIndex1] = command;
+    }
+
+    if (arg1[0] != NULL && strcmp(arg1[0], "exit") == 0) {
       exit(0);
     }
 
+    arg1[argIndex1] = NULL;
 
+    int argIndex2 = 0;
+    int pipeExist = 0;
+    int x = 0;
+    for (; argIndex2 < argIndex1; argIndex2++) {
+      if (pipeExist)
+
+      {
+        arg2[x] = arg1[argIndex2];
+        x++;
+      }
+      if (strcmp(arg1[argIndex2], "|") == 0)
+
+      {
+        arg1[argIndex2] = NULL;
+        pipeExist = 1;
+      }
+    }
+
+    pid = fork();
+
+    executeCommand(pid, pipefileDesc, pipeExist);
   }
 }
 
+void executeCommand(pid_t pid, int pipefileDesc[2], int pipeExist) {
+  if (pid < 0) {
+    perror("fork");
+    exit(1);
+  } else if (pid == 0) {
+    if (pipeExist) {
+      dup2(pipefileDesc[1], STDOUT_FILENO);
+      close(pipefileDesc[0]);
+    }
+    execvp(arg1[0], arg1);
+
+    printf("execvp has not been executed, so this statement is reached. \n");
+  } else {
+    if (!pipeExist) {
+      usleep(10000);
+      pid = wait(NULL);
+    }
+
+    if (pipeExist) {
+      pid = fork();
+      if (pid == 0) {
+        dup2(pipefileDesc[0], STDIN_FILENO);
+        close(pipefileDesc[1]);
+
+        execvp(arg2[0], arg2);
+      } else {
+        usleep(10000);
+        pid = wait(NULL);
+      }
+    }
+  }
+
+  close(pipefileDesc[0]);
+  close(pipefileDesc[1]);
+}
